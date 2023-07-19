d003f2b1c5458f03716a1e6a6f40bd8869af8bd6
Wanguifiona <fionawanguimaina@gmail.com>
2023-07-19 09:02:07 +0300

    path to check

---
+++ parser.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+/**
+ * is_cmd - checking if given path corresponds to a regular file
+ * @info: pointer to the info_t struct
+ * @path: path to check
+ *
+ * Return: 1 if the path is regular file, if not return 0
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+/**
+ * dup_chars - Creates a duplicate string from original string.
+ * @pathstr: The original string.
+ * @start: The starting index of the portion to duplicate.
+ * @stop: The stopping index (exclusive) of the portion to duplicate.
+ *
+ * Return: Pointer to the duplicated string.
+ */
+
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+/**
+ * find_path - find the full path of a command in the given path string
+ * @info: pointer to the info_t struct
+ * @pathstr: the path string to search in
+ * @cmd: the command to find
+ * Return: pointer to the full path of command if found, if not return NULL
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
