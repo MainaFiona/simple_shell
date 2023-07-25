b27f934d0db035d666eb90876e6fe9c94cbe25ff
MainaFiona <mainawanguifiona@gmail.com>
2023-07-25 21:47:20 +0000

    fix leak

---
+++ _memory.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+/**
+ * bfree - make pointer free and NULL address
+ * @ptr: pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
