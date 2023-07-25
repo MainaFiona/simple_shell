554c819a17b9bbeefa7651491f50e09eec260b04
MainaFiona <mainawanguifiona@gmail.com>
2023-07-25 20:37:49 +0000

    edit exits file

--- exits.c
+++ exits.c
@@ -9,25 +9,25 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-        int i, j;
-        char *s = dest;
-
-        i = 0;
-        while (src[i] != '\0' && i < n - 1)
+	int i, j;
+	char *s = dest;
+	
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
         {
-                dest[i] = src[i];
-                i++;
+		dest[i] = src[i];
+		i++;
         }
-        if (i < n)
+	if (i < n)
         {
-                j = i;
-                while (j < n)
+		j = i;
+		while (j < n)
                 {
-                        dest[j] = '\0';
-                        j++;
+			dest[j] = '\0';
+			j++;
                 }
         }
-        return (s);
+	return (s);
 }
 /**
  *
@@ -38,22 +38,22 @@ char *_strncpy(char *dest, char *src, int n)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        int i, j;
-        char *s = dest;
-
-        i = 0;
-        j = 0;
-        while (dest[i] != '\0')
-                i++;
-        while (src[j] != '\0' && j < n)
+	int i, j;
+	char *s = dest;
+	
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
         {
-                dest[i] = src[j];
-                i++;
-                j++;
+		dest[i] = src[j];
+		i++;
+		j++;
         }
         if (j < n)
-                dest[i] = '\0';
-        return (s);
+		dest[i] = '\0';
+	return (s);
 }
 /**
  *
@@ -64,10 +64,10 @@ char *_strncat(char *dest, char *src, int n)
  */
 char *_strchr(char *s, char c)
 {
-        do {
-                if (*s == c)
-                        return (s);
+	do {
+		if (*s == c)
+			return (s);
         } while (*s++ != '\0');
-
-        return (NULL);
+	
+	return (NULL);
 }
