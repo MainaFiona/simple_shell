cdb23ec80256c4d17594d65997622acd310df55e
MainaFiona <mainawanguifiona@gmail.com>
2023-07-25 20:43:05 +0000

    edit exits file

--- exits.c
+++ exits.c
@@ -14,19 +14,19 @@ char *_strncpy(char *dest, char *src, int n)
 	
 	i = 0;
 	while (src[i] != '\0' && i < n - 1)
-        {
+	{
 		dest[i] = src[i];
 		i++;
-        }
+	}
 	if (i < n)
-        {
+	{
 		j = i;
 		while (j < n)
-                {
+		{
 			dest[j] = '\0';
 			j++;
-                }
-        }
+		}
+	}
 	return (s);
 }
 /**
@@ -46,11 +46,11 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i] != '\0')
 		i++;
 	while (src[j] != '\0' && j < n)
-        {
+	{
 		dest[i] = src[j];
 		i++;
 		j++;
-        }
+	}
         if (j < n)
 		dest[i] = '\0';
 	return (s);
@@ -67,7 +67,7 @@ char *_strchr(char *s, char c)
 	do {
 		if (*s == c)
 			return (s);
-        } while (*s++ != '\0');
+	}	while (*s++ != '\0');
 	
 	return (NULL);
 }
