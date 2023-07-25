4496b1e6f748454f56d971ed6667c286d520b778
arnoldmn <arnoldnzibuk@gmail.com>
2023-07-25 17:34:38 +0000

    getline

---
+++ getline.c
@@ -0,0 +1,161 @@
+#include "main.h"
+/**
+ * input_buf - chain command buiffers
+ * @info: struct param
+ * @buff: buffer address
+ * @len: length of variable address
+ * Return: bytes
+ */
+ssize_t input_buf(info_t *info, char **buff, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len)
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0';
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			/* if (_strchr(*buf, ';')) is this a command chain? */
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
+/**
+ * get_input - getline without the newline
+ * @info: struct param
+ * Return: read bytes
+ */
+ssize_t get_input(info_t *info)
+{
+	static char *buff;
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buff_p = &(info->arg), *p;
+
+	_putchar(BUFF_FLUSH);
+	r = input_buff(info, &buff, &len);
+	if (r == -1)
+		return (-1);
+	if (len)
+	{
+		j = i;
+		p = buff + i;
+
+		check_chain(info, buff, &j, i, len);
+		while (j < len)
+		{
+			if (is_chain(info, buff, &j))
+				break;
+			j++;
+		}
+
+		i = j + 1;
+		if (i >= len)
+		{
+			i = len = 0;
+			info->cmd_buf_type = CMD_NORM;
+		}
+
+		*buff_p = p;
+		return (_strlen(p));
+	}
+
+	*buff_p = buff;
+	return (r);
+}
+/**
+ * read_buff - function will read a buffer
+ * @info: param struct
+ * @buff: buffer address
+ * @i: index struct
+ * Return: r
+ */
+ssize_t read_buff(info_t *info, char *buff, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buff, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+/**
+ * _getline - will receive the next line of input from STDIN
+ * @info: struct param
+ * @ptr: the address of pointer to buffer
+ * @length: the size of allocated pointer if not NULL
+ *
+ * Return: s
+ */
+int _getline(info_t *info, char **ptr, size_t *length)
+{
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p)
+		return (p ? free(p), -1 : -1);
+
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+
+	s += k - i;
+	i = k;
+	p = new_p;
+
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
+/**
+ * sigintHandler - will block ctrl
+ * @sig_num: num signal
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
